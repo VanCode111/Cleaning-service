054e0438ce6eed4bf5b525ea0dbc5bf17ee183a1
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2021-07-09 11:22:21 +0300

    init

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-const express = require('express');
-const sequelize = require('./db');
-const models = require('./models/models');
-const cors = require('cors');
-const router = require('./routes/index');
+require("dotenv").config();
+const express = require("express");
+const sequelize = require("./db");
+const models = require("./models/models");
+const cors = require("cors");
+const router = require("./routes/index");
+const errorHandle = require("./middleware/ErrorHandlingMiddleware");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', router);
+app.use("/api", router);
+app.use(errorHandle);
 
 const start = async () => {
-  try{
-    await sequelize.authenticate();
-    await sequelize.sync();
-    app.listen(PORT, () => {console.log(`Server has been started on ${PORT} port`)});
-  }catch{
-
-  }
+  try {
+    //await sequelize.authenticate();
+    //await sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server has been started on ${PORT} port`);
+    });
+  } catch {}
 };
 start();
-
